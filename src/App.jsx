08e89dae3095a94250ad9e5d3f928bfde4d943d4
imranrkhan13 <muhammadimrank034@gmail.com>
2025-12-18 10:25:52 +0530

    Inital commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Job from './job'
+function App() {
+
+  return (
+    <div>
+      < Job/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/job.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+
+function Job() {
+    const [companyname, setCompanyName] = useState("");
+    const [role, setRole] = useState("");
+    const Availablestatus = ["Applied", "Rejected", "Interview"];
+    const [status, setStatus] = useState("");
+
+    const statusMessages = {
+        Applied: "Hope it changes to interview soon!",
+        Rejected: "We go again.",
+        Interview: "Hell yeah, you got this!"
+    };
+
+    return (
+        <div>
+            <h3>Company Name: {companyname}</h3>
+            <h3>Role: {role}</h3>
+            <h3>Status: {status}</h3>
+
+            <input type="name"
+                placeholder="Enter Role"
+                value={role}
+                onChange={(e) => setRole(e.target.value)}
+            />
+            <input type="name"
+                placeholder="Enter Company Name"
+                value={companyname}
+                onChange={(e) => setCompanyName(e.target.value)}
+            />
+            <select
+                value={status}
+                onChange={(e) => setStatus(e.target.value)}>
+                <option value="">Select Status</option>
+                {Availablestatus.map((option) => (
+                    <option key={option} value={option}>
+                        {option}
+                    </option>
+                ))}
+            </select>
+            <p>{statusMessages[status]}</p>
+        </div>
+    )
+}
+export default Job;
