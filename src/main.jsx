bb04de9055e9178ce280d7b72ed30593c9b255b6
imranrkhan13 <muhammadimrank034@gmail.com>
2026-01-06 11:34:43 +0530

    d

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App";
 import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <GoogleOAuthProvider clientId="537268892470">
+        <GoogleOAuthProvider clientId="537268892470-13n9va8n3rdb98qm422lt227lc5etpcj.apps.googleusercontent.com">
             <App />
         </GoogleOAuthProvider>
     </React.StrictMode>
