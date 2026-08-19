d5cbac0cddb36e2c6596122c6f4f14c14fb8c6d2
imranrkhan13 <9.9342205e+07+imranrkhan13@users.noreply.github.com>
2026-08-19 13:43:37 +0000

    fix: remake desktop sidebar and overview

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./AppNew";
 import "./index.css";
+import "./desktop-remake.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
